5fd2d33c7b7522c17cdf9e94db665c0532d99866
Wilson Felipe <wfelipe2011@gmail.com>
2021-03-14 20:35:58 -0300

    Mudando shema

--- src/models/formulario.js
+++ src/models/formulario.js
@@ -4,14 +4,8 @@ const Schema = mongoose.Schema;
 const formulario = new Schema(
   {
     name: { type: String },
-    sobrenome: { type: String },
+    email: { type: String },
     cpf: { type: Number },
-    phone: { type: Number },
-    profissao: { type: String },
-    cep: { type: Number },
-    endereco: { type: String },
-    bairro: { type: String },
-    cidade: { type: String },
   },
   {
     timestamps: true,
